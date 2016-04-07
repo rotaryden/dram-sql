2812529cdc9554e7f5dea1b045c3d11a83382c42
altitudebreath <altitudebreath@gmail.com>
2016-04-07 13:23:28 +0300

    SQL injection safety added

--- index.js
+++ index.js
@@ -6,10 +6,20 @@ var _ = require('lodash');
  * Simple Chainable SQL expression constructor
  * @constructor
  */
-function SQL(initialSQL) {
+function SQL(initialSQL, options) {
     var t = this;
+    t.options = {
+        isSafe: false
+    };
+    if (options){
+        Object.assign(t.options, options);
+    }
     t._sql = (initialSQL || '') + ' '; //a space for attachability to other parts of the query
 }
+
+SQL.flag = {
+    SAFE: 1
+};
 
 SQL.prototype.raw = function (raw) {
     var t = this;
@@ -17,47 +27,86 @@ SQL.prototype.raw = function (raw) {
     return t;
 };
 
+
+SQL.prototype.allSafe = function () {
+    this.options.isSafe = true;
+};
+
+
+SQL.prototype._extractNames = function (properties){
+    if (_.isArray(properties)){
+            return properties;
+    } else if (_.isObject(properties)){
+         return _.keys(properties);
+    } else if (typeof properties === "string"){
+        return properties.split(/\s*,\s*/);
+    }
+    throw Error("Populated Names should be an Object, " +
+        "Array or String with comma-separated values:" + properties);
+};
+
+SQL.prototype._checkNameSafe = function (s, flags) {
+    var t = this;
+    if (! t.options.isSafe && ! (flags & SQL.flag.SAFE)) {
+        if (/^\w+$/.test(s)) {
+            throw Error("Invalid name (may be SQL injection!!!):" + s);
+        }
+    }
+};
+
+SQL.prototype._checkNamesSafe = function (names, flags) {
+    var t = this;
+    if (! t.options.isSafe && ! (flags & SQL.flag.SAFE)) {
+        var rx = /^\w+$/;
+        for (var i = 0; i < names.length; i++) {
+            if (!rx.test(names[i])) {
+                throw Error("Invalid name (may be SQL injection!!!):" + names[i] +
+                    "\n names: " + names);
+            }
+        }
+    }
+};
+
 SQL._raw = function(raw) {
-    return function (optArg) {
+    return function (optArgument, flags) {
         var t = this;
-        if (optArg && typeof optArg !== "string") throw Error("_raw: optional argument may be only string: " + optArg);
-        t._sql += ' ' + raw + ' ' + (optArg ? (optArg + ' ') : '');
+        t._checkNameSafe(optArgument, flags);
+        t._sql += ' ' + raw + ' ' + (optArgument ? (optArgument + ' ') : '');
         return t;
     };
 };
 
 //.update('table_name').set(properties)
 SQL.prototype.update = SQL._raw('UPDATE');
+
 //.insert('table_name').set(properties)
 //.insert('table_name').values(properties)
 SQL.prototype.insert = SQL._raw('INSERT INTO');
-SQL.prototype.set = function (properties) {
+
+SQL.prototype.set = function (properties, flags) {
+    //WARNING: this.eq() has protection from SQL injection,
+    //note this in case of implementation changes
     var t = this;
-    t._sql += ' SET ' + t.eq(properties, ',') + ' '; //.eq() gives right syntax 
+    t._sql += ' SET ' + t.eq(properties, ',', flags) + ' '; //.eq() gives right syntax 
     return t;
 };
 
-SQL.prototype.values = function (properties) {
+SQL.prototype.values = function (properties, flags) {
     var t = this;
-    if (! _.isObject(properties)) throw Error("Invalid VALUES properties:" + properties);
-    var keys =  _.keys(properties);
-    t._sql += ' (' + keys.join(', ') + 
-        ') VALUES ( :' + keys.join(', :') + ' )'; 
+    var names =  t._extractNames(properties);
+    t._checkNamesSafe(names, flags);
+    t._sql += ' (' + names.join(', ') + 
+        ') VALUES ( :' + names.join(', :') + ' )'; 
     return t;
 };
 
 //.select(['a', 'b'])
-SQL.prototype.select = function (columns) {
+//.select('a, b')
+SQL.prototype.select = function (columns, flags) {
     var t = this;
-    if (typeof columns === "string"){
-        columns = _.slice(arguments);
-    } else if (_.isObject(columns)) {
-        columns = _.values(columns);
-    }
-    if (! _.isArray(columns)) throw Error("Invalid SELECT columns:" + columns + 
-        "\n Use selectFrom() for SELECT * FROM");
-    
-    t._sql += ' SELECT ' + columns.join(', ') + ' '; 
+    var names =  t._extractNames(columns);
+    t._checkNamesSafe(names, flags);
+    t._sql += ' SELECT ' + names.join(', ') + ' '; 
             
     return t;
 };
@@ -68,6 +117,7 @@ SQL.prototype.end = SQL._raw(';');
 //.from('table_name') -- for single table select
 //.from().join(tables) -- for joining tables
 SQL.prototype.from = SQL._raw('FROM');
+
 //shortcut: .selectFrom().join('A', 'B')
 //.selectFrom('A')
 SQL.prototype.selectFrom = SQL._raw('SELECT * FROM');
@@ -90,17 +140,13 @@ SQL.prototype.having = SQL._raw('HAVING');
 //.limit(100)
 SQL.prototype.limit = SQL._raw('LIMIT');
 
-SQL._makeJoin = function(join) {
+SQL._makeJoin = function(join, flags) {
     return function (tables) {
         var t = this;
         if (tables) {
-            if (_.isObject(tables)){
-                tables = _.values(tables);
-            } else if (typeof tables === 'string'){
-                tables = _.slice(arguments);
-            }
-            if (! _.isArray(tables)) throw Error("Invalid JOIN tables:" + tables);
-            t._sql += ' (' + tables.join(' ' + join + ' ') + ') ';
+            var names =  t._extractNames(tables);
+            t._checkNamesSafe(names, flags);
+            t._sql += ' (' + names.join(' ' + join + ' ') + ') ';
         }else{
             //act as simple conjunction
             t._sql += ' ' + join + ' ';
@@ -114,23 +160,25 @@ SQL.prototype.left = SQL._makeJoin('LEFT JOIN');
 SQL.prototype.right = SQL._makeJoin('RIGHT JOIN');
 SQL.prototype.outer = SQL._makeJoin('FULL OUTER JOIN');
 
-SQL._makeOp = function (op) {
+SQL._makeOp = function (operation) {
     /**
      * Compiles criteria for ON , WHERE etc 
+     
+     * WARNING: "joiner" is not safe!
+     
      * @param props
      * @param joiner - can be , AND OR
+     * @param flags
      * @returns {SQL}
      * @private
      */
-    return function (properties, joiner) {
+    return function (properties, joiner, flags) {
         var t = this;
-        if (_.isObject(properties)){
-            properties = _.keys(properties);
-        } else if (! _.isArray(properties)){
-            throw Error("Invalid " + op + " properties (should be Object or Array):" + properties);
-        }
-        t._sql += ' (' +  _.map(properties, function(name) {
-            return name + ' ' + op + ' :' + name;
+        var names =  t._extractNames(properties);
+        t._checkNamesSafe(names, flags);
+        
+        t._sql += ' (' +  _.map(names, function(name) {
+            return name + ' ' + operation + ' :' + name;
         }).join(' ' + joiner + ' ') + ') ';
         return t;
     };
@@ -144,18 +192,23 @@ SQL.prototype.gt = SQL._makeOp('>');
 SQL.prototype.ge = SQL._makeOp('>=');
 
 
-SQL.prototype.in = function (el, set) {
+SQL.prototype.in = function (el, set, flags) {
     var t = this;
-    if (! _.isArray(set)) throw Error("Invalid IN set:" + set);
+    set =  t._extractNames(set);
+    t._checkNameSafe(el, flags);
+    t._checkNamesSafe(set, flags);
+    
     t._sql += ' (' + el + ' IN (' + set.join(', ') + ')) ';
     return t;
 };
 
-SQL.prototype.toString = SQL.prototype.ok = function () {
+SQL.prototype.toString = SQL.prototype.ok = SQL.prototype.end = function () {
     return this._sql;
 };
 
-
-module.exports = function (initialQuery) {
-    return new SQL(initialQuery)
+module.exports = {
+    make: function (initialQuery) {
+        return new SQL(initialQuery);
+    },
+    flag: SQL.flag
 };
